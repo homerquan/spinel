e9b92cba7598eb51f724b4084c9311d4266bd453
homerquan <lujooo@gmail.com>
2014-07-01 17:29:32 -0400

    debug

---
+++ public/js/app/views/HyperModalView.js
@@ -0,0 +1,50 @@
+// HyperModalView.js
+// -------
+define(["jquery", "backbone", "handlebars",
+        "./BaseView", "text!templates/hyper_modal.hbr"
+    ],
+
+    function($, Backbone, Handlebars, BaseView, template) {
+
+        var View = BaseView.extend({
+
+            // The DOM Element associated with this view
+            el: "#overlay-container",
+            template: Handlebars.compile(template),
+
+            // View Event Handlers
+            events: {
+                "click #btn-signin": "signin",
+                "click #btn-signup": "signup",
+                "click button.close": "close"
+            },
+
+            // Renders the view's template to the UI
+            render: function() {
+                this.$el.html(this.template());
+                // Maintains chainability
+                return this;
+            },
+
+            signin: function(e) {
+                e.preventDefault();
+                var name = this.$('#username').val();
+                var pass = this.$('#password').val();
+                this.session.set("redirectFrom", Backbone.history.fragment);
+                this.session.login(name, pass, function() {
+                    this.clear();
+                }.bind(this));
+            },
+
+            close: function() {
+                this.clear();
+            }
+
+        });
+
+        // Returns the View class
+        return View;
+
+    }
+
+);
